b0cc59e480c5cf5d0b8a4b4e7cd665c41a556cfa
huskeypm <huskeypm@gmail.com>
2015-02-16 22:06:26 -0500

    Added stuff

--- sachse.py
+++ sachse.py
@@ -41,7 +41,8 @@ class Params():
   verbose=False 
 
   # time steps 
-  steps = 500
+  T = 500
+  T = 3  
   dt = 1.0   # [ms] 
 
 
@@ -86,6 +87,58 @@ class InitialConditions(Expression):
   def value_shape(self):
     return (nDOF,)
 
+ # TODO put in real info 
+def UpdateBeardVals(nC,bM,mgatpi_uM,fadpi_uM ,fmgi_uM ):
+  
+
+  ### [mM] ###
+  print "mgatpi %f [uM] adpi %f [uM] mgi %f [uM] " % \
+   (mgatpi_uM,fadpi_uM,fmgi_uM)
+  cmgatpi = mgatpi_uM * uM_to_mM
+  cfadpi = fadpi_uM * uM_to_mM
+  cfmgi = fmgi_uM * uM_to_mM
+  
+  # get nucleotide concentreations at vertex i 
+  # convert MgATP into free ATP (used by Beard)
+  #catpi = nC.mATP_to_fATP( cmgatpi )                
+  ctatpi = nC.mATP_to_tATP( cmgatpi,cfmgi)           
+  cfatpi = ctatpi - cmgatpi 
+  ctadpi = nC.fADP_to_tADP( cfadpi,cfmgi)           
+  cmgadpi = ctadpi - cfadpi 
+
+  ## Compute nucleotide flux using beard model" 
+  # Current doesn't use [Mg]
+  jtatpi,jtadpi,dummy = bM.jNucleotide(ctatpi*mM_to_M,ctadpi*mM_to_M) # ,cmgi)
+  # multiply by '-1', since negative flux represents ATP diffusion 
+  # into cytosol 
+  jtatpi*=-1
+  jtadpi*=-1
+  #print "%f %f %f" % (ctatpi,ctadpi,jtatpi)
+
+  #jtatpi = jtadpi = 0. ; print "KILLING JATP flux"
+  # jNucleotide returns mM/s, 1 mM/s = 1000 uM/1000 ms = 1 [uM/ms]
+  print "[fatp] %f [mgatp] %f [tatp] %f [mM] jtATP %f [mM/s]\n"\
+    % ( cfatpi,cmgatpi,ctatpi,jtatpi)
+  print "[fadp] %f [mgadp] %f [tadp] %f [mM] jtATP %f [mM/s]\n"\
+    % ( cfadpi,cmgadpi,ctadpi,jtadpi)
+
+
+  # j = dN/dt --> dN = j*dt
+  # j [mM/s] <==> j[uM/ms]
+  # dt [ms] * j [uM/ms] = delatp [uM] --> delatp [mM]
+  deltatpi = params.dt * jtatpi * uM_to_mM
+  deltadpi = params.dt * jtadpi * uM_to_mM
+
+
+  # first will assume that total nucleotide is added (substracted) 
+  # from the free nucleotide; 
+  # When we add del_FreeATP to the FreeATP concentration, we push 
+  # the Mg+fATP<-->MgATP from equilibrium. Therefore, we recompute
+  # the system at equlibrium  
+  cfatpn = cfatpi + deltatpi
+  cfadpn = cfadpi + deltadpi
+  #print "fatpn %f (before equil) " % cfatpn
+
 
 def PrintSlice(mesh,u):  
 #    mesh = results.mesh
@@ -127,8 +180,10 @@ class MyEquation(NonlinearProblem):
 # In[18]:
 
 # TODO verify units 
-def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
-	   debug=True):
+def tsolve(fileName="sarcomere.xml",\
+           outName="output.pvd",\
+           mode="sachse",\
+	   debug=False): 
 
   params = Params()
   if debug:
@@ -160,7 +215,7 @@ def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
   u_0  = Function(ME)  # solution from previous converged step
 
   # split mixed functions
-  print "WARNING: not sure how to generalize"       
+  #print "WARNING: not sure how to generalize"       
   cCa_n,cBuff_n, cFluo_n = split(u_n)
   cns = [cCa_n,cBuff_n, cFluo_n]
   cCa_0,cBuff_0, cFluo_0 = split(u_0)
@@ -204,50 +259,68 @@ def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
   #Dij = diag(Dii)  # for now, but could be anisotropic
   #a =inner(Dij * grad(u), grad(v))*dx
 
-  ## TODO add realistic rate constants, put into params()
-  r0 = Constant(0.1)
-  r1 = Constant(1.) 
-  r2 = Constant(0.1)
-  r3 = Constant(1.)   
-  k3 = Constant(0.5)
-
-  ## RHS 
-  # diffusion 
+  ### RHS 
+  ## diffusion 
   Ds  = params.Ds     
   RHSis=[]
   for i in range(nDOF):
-    RHSi= -inner(Ds[i]*grad(cns[i]), grad(vs[i]))*dx
+    #RHSi= -inner(Ds[i]*grad(cns[i]), grad(vs[i]))*dx
+    RHSi= -inner(Ds[i]*grad(u_n[i]), grad(vs[i]))*dx
     RHSis.append(RHSi)
 
-  # reactions
-  #- buffer 
-  kp = params.alphabuff 
-  btot = params.Btot 
-  km = params.betabuff   # probably describe this as a state
-  Jbuff = kp*cCa_n*(btot - cBuff_n) - km*cBuff_n   # probably describe this as a state
-  RHSis[idxCa] += -Jbuff*vCa*dx
-  RHSis[idxBuff] += +Jbuff*vBuff*dx
+  ## buffreactions
+  if mode=="sachse":
+    #- buffer 
+    kp = params.alphabuff 
+    btot = params.Btot 
+    km = params.betabuff   # probably describe this as a state
+    Jbuff = kp*cCa_n*(btot - cBuff_n) - km*cBuff_n   # probably describe this as a state
+    RHSis[idxCa] += -Jbuff*vCa*dx
+    RHSis[idxBuff] += +Jbuff*vBuff*dx
   
-  #- fluo 
-  kp = params.alphafluo 
-  ftot = params.Ftot 
-  km = params.betafluo   # probably describe this as a state
-  Jfluo = kp*cCa_n*(ftot - cFluo_n) - km*cFluo_n   # probably describe this as a state
-  RHSis[idxCa] += -Jbuff*vCa*dx
-  RHSis[idxFluo] += +Jbuff*vFluo*dx
+    #- fluo 
+    kp = params.alphafluo 
+    ftot = params.Ftot 
+    km = params.betafluo   # probably describe this as a state
+    Jfluo = kp*cCa_n*(ftot - cFluo_n) - km*cFluo_n   # probably describe this as a state
+    RHSis[idxCa] += -Jfluo*vCa*dx
+    RHSis[idxFluo] += +Jfluo*vFluo*dx
   
-  #Jfluo = alphafluo * u * (Ftot - FC) - betafluo*FC   # probably describe this as a state
+    #Jfluo = alphafluo * u * (Ftot - FC) - betafluo*FC   # probably describe this as a state
 
-  # reactions 
-  # RyR
-  delta0 = 1.  # TBD
-  iryr = Expression("exp(-t)",t=0)
-  delta = 1.
-  F = 1.
-  V = 1.
-  jRyR = delta0 * iryr / (2*F*delta*V)
-  # TODO assign boundary 
-  RHSis[idxCa] += jRyR*vCa*ds()
+    ## boundary reactions 
+    # RyR
+    # TODO add NCX, etc 
+    delta0 = 1.  # TBD
+    iryr = Expression("exp(-t)",t=0)
+    delta = 1.
+    F = 1.
+    V = 1.
+    jRyR = delta0 * iryr / (2*F*delta*V)
+    # Add left/right TTs
+    RHSis[idxCa] += jRyR*vCa*ds(lMarker,domain=mesh)
+    RHSis[idxCa] += jRyR*vCa*ds(rMarker,domain=mesh)
+
+    # NCX
+    r = Constant(0.001)
+    jSarco= r
+    RHSis[idxCa] += jSarco*cns[idxCa]*vs[idxCa]*ds(slMarker,domain=mesh)
+  
+
+    # SERCA (here we assume SERCA is one of the cyto fluxes)  
+    r = Constant(0.001)
+    JCyto = r*cns[idxCa]*vs[idxCa]*dx
+    RHSis[idxCa] += JCyto
+
+  # eerything communicated through ode model 
+  #elif mode=="satin" or mode=="despa":
+  elif mode=="ode":
+    # TODO 
+    jCa = Expression("r",r=0)
+    jCa.r = 0.1
+    RHSis[idxCa] += -jCa*vCa*dx
+
+  
   
   # RyR release at R TT  
   #RHS+=  r1*q*ds(rMarker,domain=mesh)
@@ -260,13 +333,15 @@ def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
   #  RHS+= -r2*u*q*ds(slMarker,domain=mesh) 
   
     
-  ## LHS    
+  ### LHS    
   dt = params.dt
   #LHS = (cCa_n-cCa_0)*vCa/dt * dx - RHSCa
   LHSis=[]
   L=0
   for i in range(nDOF):
-    LHSi= (cns[i]-c0s[i])*vs[i]/dt * dx - RHSis[i]     
+    # TODO DBL check 
+    #LHSi= (cns[i]-c0s[i])*vs[i]/dt * dx - RHSis[i]     
+    LHSi= (u_n[i]-u_0[i])*vs[i]/dt * dx - RHSis[i]     
     LHSis.append(RHSi)
     L+= LHSi
   #i = 0 
@@ -323,14 +398,22 @@ def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
       vol = assemble(Constant(1.)*dx(domain=mesh))
       assembles=np.zeros(nDOF)
       for i in range(nDOF):
-        assembles[i] = assemble(cns[i]*dx(domain=mesh))
+        #assembles[i] = assemble(cns[i]*dx(domain=mesh))
+        assembles[i] = assemble(u_n[i]*dx(domain=mesh))
       concis  = assembles/vol  
  
       ts.append(t0)
       concs.append(concis)
 
       # update
-      iryr.t = t 
+      if mode=="sachse":
+        iryr.t = t 
+      if mode=="ode":
+        # TODO run ODE model 
+        # aggregate state variables
+        # need to assemble concs over entire vol 
+        #def UpdateBeardVals(nC,bM,mgatpi_uM,fadpi_uM ,fmgi_uM ):
+        jCa.r  = 0. # TODO update with ode 
       t += dt
 
 
@@ -343,8 +426,8 @@ def tsolve(fileName="sarcomere.xml",           outName="output.pvd",
 def whosalive():
   print "I'm alive"
 
-def doit():
-  tsolve()
+def doit(debug=True,mode=""):  
+  tsolve(debug=debug,mode=mode)
 #!/usr/bin/env python
 import sys
 ##################################
@@ -393,9 +476,14 @@ if __name__ == "__main__":
   # Loops over each argument in the command line 
   for i,arg in enumerate(sys.argv):
     # calls 'doit' with the next argument following the argument '-validation'
-    if(arg=="-test"):
+    if(arg=="-debug"):
       #arg1=sys.argv[i+1] 
-      doit()     
+      doit(debug=True)     
+    elif(arg=="-test0"):
+      doit(debug=False)    
+      quit()
+    elif(arg=="-test1"):
+      doit(debug=False,mode="ode")    
       quit()
   
 
