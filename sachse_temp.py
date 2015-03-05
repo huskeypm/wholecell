6a843e1e281c930972ec8bed78234e6bb7e31ef5
huskeypm <huskeypm@gmail.com>
2015-03-05 16:34:03 -0500

    Subbing in names

--- sachse_temp.py
+++ sachse_temp.py
@@ -11,145 +11,157 @@ class Boundary(SubDomain):
     #print left 
     return left and on_boundary
 
-if 1: 
+class Params():
+  useMPI = True
+  paraview = True
+  verbose=False
+
+  Dsu = Constant(1.0) # [um^2/ms]
+  Drs = Constant(1.0)
+  dist = Constant(1.0) # distance between compartments [um] 
+
+
+  # Init conditions
+  cCaSSLInit = 0.1  # [uM] 
+  cCaCleftInit = 1.0
+  cCaInit = 10.
+
+  volSSL = Constant(2.) # [um^3] *float(volCyto))
+  volCleft = Constant(4.) # [um^3] *float(volSSL))
+
+
+def tsolve(outName="output.pvd",\
+           mode="sachse2TT", # sachse2TT, sachse4TT, ode, bcs \ 
+           params = Params(),\
+           hdfName = "out.h5",
+           debug=False):
+
   #mesh = UnitCubeMesh(8,8,8)
   mesh = UnitSquareMesh(5,5) # works for 2D too 
   dims = mesh.ufl_cell().geometric_dimension()
+
+
+
   subdomains = MeshFunction("size_t",mesh,dims-1)
   face_marker = 10
   boundary = Boundary()
   boundary.mark(subdomains,face_marker)
+  ds = Measure("ds")[subdomains]
+  dx = Measure("dx")
   ds = ds(domain=mesh,subdomain_data=subdomains)
   dx = dx(domain=mesh)
 
-# FiniteElements
-V = FunctionSpace(mesh, "CG", 1)
-R = FunctionSpace(mesh, "R", 0)
-ME = MixedFunctionSpace([V,R,R])
+  # FiniteElements
+  V = FunctionSpace(mesh, "CG", 1)
+  R = FunctionSpace(mesh, "R", 0)
+  ME = MixedFunctionSpace([V,R,R])
+  
+  vCa, vCaCleft, vCaSSL = TestFunctions(ME)
+  
+  U_n = Function(ME)
+  U_0 = Function(ME)
+  cCa_n, cCaCleft_n, cCaSSL_n = split(U_n)
+  cCa_0, cCaCleft_0, cCaSSL_0 = split(U_0)
+  
+  # Params
+  mode = "boundaryFlux" 
+  Du = Constant(1.0)
+  
+  # intercompartment transport 
+  Dsu = params.Dsu
+  Drs = params.Drs
+  dist = params.dist
+  
+  # iterator
+  dt = Constant(1.0)  # [ms]  
+  tstop = 20. 
+  
+  # volume/area info 
+  volCyto = Constant(assemble(Constant(1.0)*dx()))
+  #area_u = Constant(assemble(Constant(1.0)*ds(face_marker)))
+  volSSL = params.volSSL
+  volCleft= params.volCleft
+  volFrac_CytoSSL= volCyto/volSSL
+  volFrac_SSLCyto= 1/volFrac_CytoSSL
+  volFrac_SSLClft= volSSL/volCleft
+  volFrac_ClftSSL= 1/volFrac_SSLClft
+  volFrac_ClftCyto= volCleft/volCyto
+  volFrac_CytoClft= 1/volFrac_ClftCyto           
+  
+  
+  # System
+  
+  ## PDE 
+  # Time derivative and diffusion of field species
+  F = ((cCa_n-cCa_0)*vCa/dt + Du*inner(grad(cCa_n), grad(vCa)))*dx()
+  
+  # Flux to mesh domain from scalar domain s 
+  jFlux_SSLCyto = Dsu*(cCaCleft_n-cCa_n)/dist # note that this is multiplied by ds(face_marker) below
+  F += -jFlux_SSLCyto*vCa*ds(face_marker)
+  
+  ## Scalar s
+  # Time derivative of scalar species 
+  F += (cCaCleft_n-cCaCleft_0)/dt*vCaCleft*dx() 
+  
+  # flux from scalar domain s to mesh 
+  F += jFlux_SSLCyto*volFrac_CytoSSL*vCaCleft*ds(face_marker)
+  
+  # flux to scalar domain r to s
+  jFlux_ClftSSL =Drs*(cCaSSL_n-cCaCleft_n)/dist 
+  F += -jFlux_ClftSSL*volFrac_CytoSSL*vCaCleft*dx()
+  
+  
+  ## Scalar r
+  F += (cCaSSL_n-cCaSSL_0)/dt*vCaSSL*dx()
+  F +=  jFlux_ClftSSL*volFrac_CytoClft*vCaSSL*dx()
+  
+  # Init conditions
+  U_0.interpolate(Constant((params.cCaInit, params.cCaCleftInit, params.cCaSSLInit)))
+  
+  def conservation():
+      ## PKH test
+      acCaSSL_n = assemble(cCaSSL_n*volFrac_ClftCyto*dx())
+      ccCaSSL_n = assemble(cCaSSL_n*volFrac_ClftCyto/volCleft*dx())
+      acCaCleft_n = assemble(cCaCleft_n*volFrac_SSLCyto*dx())
+      ccCaCleft_n = assemble(cCaCleft_n*volFrac_SSLCyto/volSSL*dx())
+      acCa_n = assemble(cCa_n*dx())
+      ccCa_n = assemble(cCa_n/volCyto*dx())
+      print "cCa_n " , acCa_n, "conc ", ccCa_n
+      print "cCaCleft_n " , acCaCleft_n, "conc ", ccCaCleft_n
+      print "cCaSSL_n " , acCaSSL_n, "conc ", ccCaSSL_n
+      conserved = acCaCleft_n+acCaSSL_n+acCa_n
+      print "CONSERVATION:", conserved
+      return conserved
 
-v, p, n = TestFunctions(ME)
+  
+  
+      
+  U_n.vector()[:] = U_0.vector()[:]
+  conserved_t0 = conservation()
+  
+  
+  
+  t=0
+  U_plot = Function(V)
+  #plot(U_plot, interactive=True, range_min=min_value, range_max=max_value)
+  while t < tstop+DOLFIN_EPS:
+      solve(F==0, U_n)
+      U_plot.assign(U_n.sub(0, deepcopy=True))
+      #plot(U_plot, interactive=False, range_min=min_value, range_max=max_value)
+      #print "CONSERVATION:", assemble(cCa_n*dx())+assemble(cCaCleft_n/volFrac_CytoSSL*dx())
+      #print "CONSERVATION:", assemble(cCa_n*dx())+assemble(cCaCleft_n*volFrac_SSLCyto*dx())
+      print "############### ", t
+      conserved_ti = conservation()
+  
+      U_0.assign(U_n)
+      t += float(dt)
+  
+  
+  message = "%f | %f " % (conserved_ti , conserved_t0)
+  assert( abs(conserved_ti - conserved_t0) < 1e-3 ), message
+  #assert( abs(acCaCleft_n+acCaSSL_n+acCa_n - 60000) < 1e-5)
+  print "PASS"
+  
+  #plot(U_plot, interactive=True, range_min=min_value, range_max=max_value)
 
-U_n = Function(ME)
-U_0 = Function(ME)
-u_n, s_n, r_n = split(U_n)
-u_0, s_0, r_0 = split(U_0)
-
-# Params
-mode = "volumeSource"
-mode = "boundaryFlux" 
-Du = Constant(1.0)
-
-# intercompartment transport 
-if mode == "boundaryFlux":
-  Dsu = Constant(1.0)
-  Drs = Constant(1.0)
-  dist = Constant(1.0) # distance between compartments 
-elif mode == "volumeSource":
-  jv_us = 0.0 # volume 'flux' rate into u from s [uM/ms] (e.g. applied over dx) 
-  jv_sr = 0.0 # flux rate into s from r [uM/ms] 
-
-# iterator
-dt = Constant(1.0)   
-tstop = 20. 
-# Init conditions
-scalar_w = 0.1
-scalar_0 = 1.0
-field_0 = 10.
-
-# volume/area info 
-volume_u = Constant(assemble(Constant(1.0)*dx()))
-area_u = Constant(assemble(Constant(1.0)*ds(face_marker)))
-volume_s = Constant(2.*float(volume_u))
-volume_r = Constant(4.*float(volume_s))
-volFrac_us= volume_u/volume_s
-volFrac_su= 1/volFrac_us
-volFrac_sr= volume_s/volume_r
-volFrac_rs= 1/volFrac_sr
-volFrac_ru= volume_r/volume_u
-volFrac_ur= 1/volFrac_ru           
-
-
-# System
-
-## PDE 
-# Time derivative and diffusion of field species
-F = ((u_n-u_0)*v/dt + Du*inner(grad(u_n), grad(v)))*dx()
-if mode=="volumeSource":
-  F+= -jv_us*v*dx()
-
-# Flux to mesh domain from scalar domain s 
-jFlux_su = Dsu*(s_n-u_n)/dist # note that this is multiplied by ds(face_marker) below
-if mode=="boundaryFlux":
-  F += -jFlux_su*v*ds(face_marker)
-
-## Scalar s
-# Time derivative of scalar species 
-F += (s_n-s_0)/dt*p*dx() 
-
-# flux from scalar domain s to mesh 
-if mode=="volumeSource":
-  F += jv_us*volFrac_us* p*dx()
-  F += -jv_sr*volFrac_us* p*dx()
-
-if mode=="boundaryFlux":
-  F += jFlux_su*volFrac_us*p*ds(face_marker)
-
-# flux to scalar domain r to s
-jFlux_rs =Drs*(r_n-s_n)/dist 
-if mode=="boundaryFlux":
-  F += -jFlux_rs*volFrac_us*p*dx()
-
-
-## Scalar r
-F += (r_n-r_0)/dt*n*dx()
-if mode=="volumeSource":
-  F +=  jv_sr*volFrac_ur* n*dx()
-if mode=="boundaryFlux":
-  F +=  jFlux_rs*volFrac_ur*n*dx()
-
-# Init conditions
-U_0.interpolate(Constant((field_0, scalar_0, scalar_w)))
-
-def conservation():
-    ## PKH test
-    ar_n = assemble(r_n*volFrac_ru*dx())
-    cr_n = assemble(r_n*volFrac_ru/volume_r*dx())
-    as_n = assemble(s_n*volFrac_su*dx())
-    cs_n = assemble(s_n*volFrac_su/volume_s*dx())
-    au_n = assemble(u_n*dx())
-    cu_n = assemble(u_n/volume_u*dx())
-    print "u_n " , au_n, "conc ", cu_n
-    print "s_n " , as_n, "conc ", cs_n
-    print "r_n " , ar_n, "conc ", cr_n
-    conserved = as_n+ar_n+au_n
-    print "CONSERVATION:", conserved
-    return conserved
-
-    
-U_n.vector()[:] = U_0.vector()[:]
-conserved_t0 = conservation()
-
-
-
-t=0
-U_plot = Function(V)
-#plot(U_plot, interactive=True, range_min=min_value, range_max=max_value)
-while t < tstop+DOLFIN_EPS:
-    solve(F==0, U_n)
-    U_plot.assign(U_n.sub(0, deepcopy=True))
-    #plot(U_plot, interactive=False, range_min=min_value, range_max=max_value)
-    #print "CONSERVATION:", assemble(u_n*dx())+assemble(s_n/volFrac_us*dx())
-    #print "CONSERVATION:", assemble(u_n*dx())+assemble(s_n*volFrac_su*dx())
-    print "############### ", t
-    conserved_ti = conservation()
-
-    U_0.assign(U_n)
-    t += float(dt)
-
-
-message = "%f | %f " % (conserved_ti , conserved_t0)
-assert( abs(conserved_ti - conserved_t0) < 1e-3 ), message
-#assert( abs(as_n+ar_n+au_n - 60000) < 1e-5)
-print "PASS"
-
-#plot(U_plot, interactive=True, range_min=min_value, range_max=max_value)
+tsolve()
