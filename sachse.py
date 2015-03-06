44b3bcc5de3108ad1e5695c5d77dcb204dc56852
huskeypm <huskeypm@gmail.com>
2015-03-06 07:39:03 -0500

    Fixed issue with cleft/ssl not being coupled/uncoulped correctly

--- sachse.py
+++ sachse.py
@@ -16,7 +16,8 @@ class Params():
   verbose=False
 
   D_SSLCyto = Constant(1.0) # [um^2/ms]
-  D_ClftSSL = Constant(1.0)
+  D_CleftSSL = Constant(1.0)
+  D_CleftCyto= Constant(1.0)
   dist = Constant(1.0) # distance between compartments [um] 
 
 
@@ -25,8 +26,8 @@ class Params():
   cCaSSLInit = 1.0
   cCaInit = 10.
 
-  volSSL = 2. #Constant(2.) # [um^3] *float(volCyto))
-  volCleft = 4. #  Constant(4.) # [um^3] *float(volSSL))
+  volSSL = 100. #Constant(2.) # [um^3] *float(volCyto))
+  volCleft = 1. #  Constant(4.) # [um^3] *float(volSSL))
 
 
 def tsolve(outName="output.pvd",\
@@ -37,17 +38,13 @@ def tsolve(outName="output.pvd",\
            debug=False):
 
   #mesh = UnitCubeMesh(8,8,8)
+  ssl=True
   if "2D" in mode:
-    if mode=="2D_SSL":
-      import sarcomere2DSSL
-      cm = sarcomere2DSSL.Sarcomere2DSSL(params=params)
+    import sarcomere2DSSL
+    cm = sarcomere2DSSL.Sarcomere2DSSL(params=params)
 
-    if mode=="2D_noSSL":
-      raise RuntimeError("not tet implemented") 
-        # p/rxn for SSL 
-      import sarcomere2DwoSSL
-      cm = sarcomere2DwoSSL.sarcomere2DwoSSL(params=params)
-
+  if "noSSL" in mode:
+    ssl=False
 
   cm.params.nDOF = cm.nDOF # goes in master class 
   mesh = cm.GetMesh()
@@ -92,12 +89,13 @@ def tsolve(outName="output.pvd",\
   
   # intercompartment transport 
   D_SSLCyto = params.D_SSLCyto
-  D_ClftSSL = params.D_ClftSSL
+  D_CleftSSL = params.D_CleftSSL
+  D_CleftCyto = params.D_CleftCyto
   dist = params.dist
   
   # iterator
   dt = Constant(1.0)  # [ms]  
-  tstop = 20. 
+  tstop = 1.  
   
   # volume/area info 
   volCyto = params.volCyto
@@ -107,11 +105,14 @@ def tsolve(outName="output.pvd",\
   volCleft= params.volCleft
   volFrac_CytoSSL= volCyto/volSSL
   volFrac_SSLCyto= 1/volFrac_CytoSSL
-  volFrac_SSLClft= volSSL/volCleft
-  volFrac_ClftSSL= 1/volFrac_SSLClft
-  volFrac_ClftCyto= volCleft/volCyto
-  volFrac_CytoClft= 1/volFrac_ClftCyto           
-  
+  volFrac_SSLCleft= volSSL/volCleft
+  volFrac_CleftSSL= 1/volFrac_SSLCleft
+  volFrac_CleftCyto= volCleft/volCyto
+  volFrac_CytoCleft= 1/volFrac_CleftCyto           
+
+# System
+  ssl = False
+  #ssl = True
   
   # System
   
@@ -119,33 +120,50 @@ def tsolve(outName="output.pvd",\
   # Time derivative and diffusion of field species
   F = ((cCa_n-cCa_0)*vCa/dt + Du*inner(grad(cCa_n), grad(vCa)))*dx()
   
-  # Flux to mesh domain from scalar domain s 
-  jFlux_SSLCyto = D_SSLCyto*(cCaSSL_n-cCa_n)/dist # note that this is multiplied by ds(face_marker) below
-  F += -jFlux_SSLCyto*vCa*ds(face_marker)
+  if ssl:
+    # Flux to mesh domain from scalar domain s 
+    jFlux_SSLCyto = D_SSLCyto*(cCaSSL_n-cCa_n)/dist # note that this is multiplied by ds(face_marker) below
+    F += -jFlux_SSLCyto*vCa*ds(face_marker)
+  else: 
+    # Flux to mesh domain from scalar domain s 
+    jFlux = D_CleftCyto*(cCaCleft_n-cCa_n)/dist
+
+    F += -jFlux*vCa*ds(face_marker)
+
+
   
-  ## Scalar s
-  # Time derivative of scalar species 
+  ## Scalar SSL (even if ssl=False, we still define a PDE just ot keep the code more simple) 
   F += (cCaSSL_n-cCaSSL_0)/dt*vCaSSL*dx() 
   
-  # flux from scalar domain s to mesh 
-  F += jFlux_SSLCyto*volFrac_CytoSSL*vCaSSL*ds(face_marker)
+  if ssl:
+    # flux from scalar domain s to mesh 
+    F += jFlux_SSLCyto*volFrac_CytoSSL*vCaSSL*ds(face_marker)
   
-  # flux to scalar domain r to s
-  jFlux_ClftSSL =D_ClftSSL*(cCaCleft_n-cCaSSL_n)/dist 
-  F += -jFlux_ClftSSL*volFrac_CytoSSL*vCaSSL*dx()
+    # flux to scalar domain r to s
+    jFlux_CleftSSL =D_CleftSSL*(cCaCleft_n-cCaSSL_n)/dist 
+    F += -jFlux_CleftSSL*volFrac_CytoSSL*vCaSSL*dx()
+  else:
+    1 # do nothing 
   
   
-  ## Scalar r
+  ## Scalar cleft
   F += (cCaCleft_n-cCaCleft_0)/dt*vCaCleft*dx()
-  F +=  jFlux_ClftSSL*volFrac_CytoClft*vCaCleft*dx()
+  if ssl:
+    # flux between SSL/cleft
+    F +=  jFlux_CleftSSL*volFrac_CytoCleft*vCaCleft*dx()
+  else: 
+    # flux between SSL/cleft
+    F += volFrac_CytoCleft*jFlux*vCaCleft*ds(face_marker)
+
+
   
   # Init conditions
   U_0.interpolate(Constant((params.cCaInit, params.cCaSSLInit, params.cCaCleftInit)))
   
   def conservation():
       ## PKH test
-      acCaCleft_n = assemble(cCaCleft_n*volFrac_ClftCyto*dx())
-      #ccCaCleft_n = assemble(cCaCleft_n*volFrac_ClftCyto/volCleft*dx())
+      acCaCleft_n = assemble(cCaCleft_n*volFrac_CleftCyto*dx())
+      #ccCaCleft_n = assemble(cCaCleft_n*volFrac_CleftCyto/volCleft*dx())
       # below is shorthand, but i left comment above so its clear where it originated 
       ccCaCleft_n = assemble(cCaCleft_n*volCyto*dx())
       acCaSSL_n = assemble(cCaSSL_n*volFrac_SSLCyto*dx())
@@ -226,6 +244,7 @@ def tsolve(outName="output.pvd",\
   hdf.close()
   
   
+  # Check that the gods of transport are happy 
   if doAssert:
     message = "%f | %f " % (conserved_ti , conserved_t0)
     assert( abs(conserved_ti - conserved_t0) < 1e-3 ), message
@@ -234,4 +253,99 @@ def tsolve(outName="output.pvd",\
   
   #plot(U_plot, interactive=True, range_min=min_value, range_max=max_value)
 
-tsolve()
+# In[19]:
+def whosalive():
+  print "I'm alive!"
+
+def doit(debug=True,mode="",params=Params(),hdfName="a.h5"):  
+  tsolve(debug=debug,mode=mode,params=params,hdfName=hdfName)
+#!/usr/bin/env python
+import sys
+##################################
+#
+# Revisions
+#       10.08.10 inception
+#
+##################################
+
+
+#
+# Message printed when program run without arguments 
+#
+def helpmsg():
+  scriptName= sys.argv[0]
+  msg="""
+Purpose: 
+ 
+Usage:
+"""
+  msg+="  %s -validation" % (scriptName)
+  msg+="""
+  
+ 
+Notes:
+
+"""
+  return msg
+
+#
+# MAIN routine executed when launching this script from command line 
+#
+if __name__ == "__main__":
+  import sys
+  msg = helpmsg()
+  remap = "none"
+
+  if len(sys.argv) < 2:
+      raise RuntimeError(msg)
+
+  #fileIn= sys.argv[1]
+  #if(len(sys.argv)==3):
+  #  1
+  #  #print "arg"
+
+  # Loops over each argument in the command line 
+  params = Params()
+  params.T = 10 
+  for i,arg in enumerate(sys.argv):
+    # calls 'doit' with the next argument following the argument '-validation'
+    if(arg=="-debug"):
+      #arg1=sys.argv[i+1] 
+      doit(debug=True)     
+    elif(arg=="-test0"):
+      doit(debug=False)    
+      quit()
+    elif(arg=="-test1"):
+      tag = "ode"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+    elif(arg=="-test2D"):
+      tag = "2D_SSL"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+    elif(arg=="-test2Dno"):
+      tag = "2D_noSSL"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+    elif(arg=="-testsatin"):
+      tag = "satin"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+    elif(arg=="-test2"):
+      tag = "sachse2TT"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+    elif(arg=="-test4"):
+      tag = "sachse4TT"
+      doit(debug=False,params=params,hdfName=tag+".h5",mode=tag)
+      quit()
+
+
+
+
+
+
+  raise RuntimeError("Arguments not understood")
+
+
+
