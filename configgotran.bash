9008666568c48b0f425300e347cbf413b5859f0c
Peter Kekenes-Huskey <pkekeneshuskey@uky.edu>
2016-02-23 13:23:03 -0500

    Slight path mod

--- configgotran.bash
+++ configgotran.bash
@@ -1,10 +1,8 @@
-export MYPATH=/home/huskeypm/sources/mypython/
-export LOC=/home/huskeypm/sources/
+export MYPATH=/home/AD/pmke226/sources/mypython/
+export LOC=/home/AD/pmke226/sources/
 export PYTHONPATH=$PYTHONPATH:$MYPATH/lib/python2.7/site-packages/
-
+export PATH=$PATH:$LOC/gotran/scripts/
 python -c "import instant"
 python -c "import modelparameters"
 python -c "import gotran"
-export PYTHONPATH=$PYTHONPATH:/home/huskeypm/sources/mypython//lib/python2.7/site-packages/
-export PATH=$PATH:$LOC/gotran/scripts/
 
