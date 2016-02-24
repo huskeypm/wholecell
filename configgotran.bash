6d65d3a8b017361b84b979393ab3986107a5cfc6
Caitlin Scott <cescott111@aol.com>
2016-02-23 21:25:40 -0500

    Changes to figures.

--- configgotran.bash
+++ configgotran.bash
@@ -1,5 +1,5 @@
-export MYPATH=/home/AD/pmke226/sources/mypython/
-export LOC=/home/AD/pmke226/sources/
+export LOC=/u1/pmke226/srcs/            
+export MYPATH=$LOC/mypython/
 export PYTHONPATH=$PYTHONPATH:$MYPATH/lib/python2.7/site-packages/
 export PATH=$PATH:$LOC/gotran/scripts/
 python -c "import instant"
