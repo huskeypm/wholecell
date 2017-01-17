da750315b989bc4bd6bba4e5182908dbde50c5a1
Peter Kekenes-Huskey <pkekeneshuskey@uky.edu>
2017-01-17 09:36:44 -0500

    partially fixed

--- fitting_sensitivity/fitter.py
+++ fitting_sensitivity/fitter.py
@@ -19,16 +19,17 @@ import numpy as np
 ##
 generation =1
 progenyNumber= 2
-name = "gen%dprog%d"%(generation, progenyNumber)
-varDict = dict()
-stateDict = dict()
 odeName = "../shannon_2004.ode"
 #odeName = "../simpleRyR.ode"
 
-
+#
+# Launches an ode simulation on a single core 
+#
 def worker(procnum):
     print 'I am number %d in process %d' % (procnum, getpid())
     name = "gen%dprog%d"%(generation, progenyNumber)
+    varDict = dict()
+    stateDict = dict()
     varName = "PCa"
     varVal  = 1.
     varDict[varName] = np.float(varVal)
@@ -37,13 +38,58 @@ def worker(procnum):
     return procnum,getpid()
 
 
-def doit(processes=3,jobs=5):
-    pool = multiprocessing.Pool(processes = processes)
+#
+# Simply launches #jobs of #numCores and collects results
+#
+def SimpleRun(numCores=3,jobs=5):
+    pool = multiprocessing.Pool(numCores = numCores)
     #print pool.map(worker, range(5))
     outputs = dict( pool.map(worker, range(jobs)) )
     
     for key, value in outputs.iteritems():
       print key,value
+
+#
+# Launches randomized parameters and collects 
+# results 
+#
+def ParameterSensitivity(
+  numCores=5,  # number of cores over which jobs are run
+  numRandomDraws=3  # number of random draws for each parameter
+  ):
+  
+  # Define parameters to vary and their default 
+  # values  
+  # should probably initialize ode and grab 
+  # defaults. Lazy here
+  parmDict = dict()
+
+  defaultVal = 5.4e-4; stdDev = 1e-5
+  parmDict["PCa"] = ( defaultVal, stdDev )
+
+  defaultVal = 5.311e-3; stdDev = 1e-4
+  parmDict["V_max_Jpump"] = ( defaultVal, stdDev )
+  
+
+  # Create a list of jobs with randomized parameters
+  jobList = []
+  
+  for key,values in parmDict.iteritems():
+    print key
+    
+    ## generate random pertubrations
+    # draw from normal distribution
+    mu,sigma = values
+    randomDraws = np.random.normal(mu,sigma,numRandomDraws)
+
+    # create a list of jobs 
+    print randomDraws
+    listN = [{key,val} for val in randomDraws]
+    jobList+=listN
+    
+
+  print jobList
+
 
 
 #!/usr/bin/env python
@@ -98,12 +144,15 @@ if __name__ == "__main__":
 
   # Loops over each argument in the command line 
   for i,arg in enumerate(sys.argv):
-    # calls 'doit' with the next argument following the argument '-validation'
-    if(arg=="-run"):
+    # calls 'SimpleRun' with the next argument following the argument '-validation'
+    if(arg=="-simpleRun"):
       #arg1=sys.argv[i+1] 
-      #doit(arg1)
-      doit(processes=5,jobs=5)
+      #SimpleRun(arg1)
+      SimpleRun(numCores=5,jobs=5)
+      exit()
 
+    if(arg=="-parameterSensitivity"):
+      ParameterSensitivity(numCores=5)
       exit()
   
 
